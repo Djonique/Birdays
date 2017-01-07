e9f449d1f6603dc0726b636365107ce15fe643cf
Evgeny Timofeev <mailsoset@gmail.com>
2017-01-07 10:51:23 +0300

    Utils refactor

--- app/src/main/java/com/djonique/birdays/utils/Utils.java
+++ app/src/main/java/com/djonique/birdays/utils/Utils.java
@@ -6,23 +6,35 @@ import java.util.Locale;
 
 public class Utils {
 
+    private static Calendar today = Calendar.getInstance();
+    private static Calendar dayOfBirthday = Calendar.getInstance();
+
     public static String getDate(long date) {
         DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.DEFAULT, Locale.getDefault());
         return dateFormat.format(date);
     }
 
+    private static int getDay(Calendar calendar) {
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    private static int getMonth(Calendar calendar) {
+        return calendar.get(Calendar.MONTH);
+    }
+
+    private static int getYear(Calendar calendar) {
+        return calendar.get(Calendar.YEAR);
+    }
+
     public static int getAge(long date) {
-        Calendar birthdayDate = Calendar.getInstance();
-        Calendar today = Calendar.getInstance();
+        dayOfBirthday.setTimeInMillis(date);
 
-        birthdayDate.setTimeInMillis(date);
+        int age = getYear(today) - getYear(dayOfBirthday);
 
-        int age = today.get(Calendar.YEAR) - birthdayDate.get(Calendar.YEAR);
-
-        if (today.get(Calendar.MONTH) < birthdayDate.get(Calendar.MONTH)) {
+        if (getMonth(today) < getMonth(dayOfBirthday)) {
             age--;
-        } else if ((today.get(Calendar.MONTH) == birthdayDate.get(Calendar.MONTH)) &&
-                (today.get(Calendar.DAY_OF_MONTH) < birthdayDate.get(Calendar.DAY_OF_MONTH))) {
+        } else if (getMonth(today) == getMonth(dayOfBirthday) &&
+                getDay(today) < getDay(dayOfBirthday)) {
             age--;
         }
         return age;
@@ -31,31 +43,22 @@ public class Utils {
     public static boolean isCurrentMonth(long date) {
         boolean thisMonth = false;
 
-        Calendar today = Calendar.getInstance();
-        Calendar dayOfBirthday = Calendar.getInstance();
-
         dayOfBirthday.setTimeInMillis(date);
 
-        if (dayOfBirthday.get(Calendar.MONTH) == today.get(Calendar.MONTH)) {
+        if (getMonth(dayOfBirthday) == getMonth(today)) {
             thisMonth = true;
         }
-
         return thisMonth;
     }
 
     public static boolean isCurrentDay(long date) {
         boolean thisDay = false;
 
-        Calendar today = Calendar.getInstance();
-        Calendar dayOfBirthday = Calendar.getInstance();
-
         dayOfBirthday.setTimeInMillis(date);
 
-        if (dayOfBirthday.get(Calendar.DAY_OF_MONTH) == today.get(Calendar.DAY_OF_MONTH) &&
-                isCurrentMonth(date)) {
+        if (getDay(dayOfBirthday) == getDay(today) && isCurrentMonth(date)) {
             thisDay = true;
         }
-
         return thisDay;
     }
 }
