ccbb8568690430b63bcc1ee3970302f2cdbca3a5
Evgeny Timofeev <mailsoset@gmail.com>
2017-01-08 23:09:32 +0300

    added DetailActivity menu

--- app/src/main/java/com/djonique/birdays/activities/DetailActivity.java
+++ app/src/main/java/com/djonique/birdays/activities/DetailActivity.java
@@ -121,7 +121,7 @@ public class DetailActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar_back_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
         return true;
     }
 
